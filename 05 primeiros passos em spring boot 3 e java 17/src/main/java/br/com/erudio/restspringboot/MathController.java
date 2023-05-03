eb33122d1aa2d784d0cb3ff0a6d9465924a4c340
Sandro Livino <sandro.livino@gmail.com>
2023-05-03 14:42:10 -0300

    Cap. 06 - Simple Math sem tratamento de exceções

---
+++ 05 primeiros passos em spring boot 3 e java 17/src/main/java/br/com/erudio/restspringboot/MathController.java
@@ -0,0 +1,32 @@
+package br.com.erudio.restspringboot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+    }
+
+    public static Double covertToDouble(String strNumber) {
+        if (strNumber == null) return 0d;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) {return Double.parseDouble(number);}
+    else return 0d;
+
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
