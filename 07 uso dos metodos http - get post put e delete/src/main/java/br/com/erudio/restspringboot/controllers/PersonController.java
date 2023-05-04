eaadc5f9eed67e51978873e58d8b2530eb8782a2
Sandro Livino <sandro.livino@gmail.com>
2023-05-04 14:19:25 -0300

    07 Uso dos métodos http - Utilização dos verbos POST, PUT e DELETE com mock (sem acesso ao BD)

--- 07 uso dos metodos http - get post put e delete/src/main/java/br/com/erudio/restspringboot/controllers/PersonController.java
+++ 07 uso dos metodos http - get post put e delete/src/main/java/br/com/erudio/restspringboot/controllers/PersonController.java
@@ -4,10 +4,7 @@ import br.com.erudio.restspringboot.model.Person;
 import br.com.erudio.restspringboot.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.List;
@@ -32,5 +29,28 @@ public class PersonController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return service.findAll();
+    }
+
+    @RequestMapping(
+            method=RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method=RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(
+            value = "/{id}",
+            method=RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
     }
 }
