c884689e5712e79aec3ed7448c645c2ceaa89573
Sandro Livino <sandro.livino@gmail.com>
2023-05-03 22:31:40 -0300

    Ajustes na Classe GreetingController

--- 07 uso dos metodos http - get post put e delete/src/main/java/br/com/erudio/restspringboot/controllers/GreetingController.java
+++ 07 uso dos metodos http - get post put e delete/src/main/java/br/com/erudio/restspringboot/controllers/GreetingController.java
@@ -10,11 +10,12 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Contador: %s - Saudação: Hello, %s %s!";
     private final AtomicLong id = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(id.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+                             @RequestParam(value = "surname") String surname) {
+        return new Greeting(id.incrementAndGet(), String.format(template, id, name, surname));
     }
 }
