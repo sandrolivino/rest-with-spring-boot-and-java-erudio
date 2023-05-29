242c7bc253301b7c594a55d8670051f710d391f8
Sandro Livino <sandro.livino@gmail.com>
2023-05-29 18:17:06 -0300

    13 Content Negotiation - MediaType YAML

--- 13 content negotiation/src/main/java/br/com/erudio/restspringboot/config/WebConfig.java
+++ 13 content negotiation/src/main/java/br/com/erudio/restspringboot/config/WebConfig.java
@@ -1,13 +1,23 @@
 package br.com.erudio.restspringboot.config;
 
+import br.com.erudio.restspringboot.serializationconverter.YamlJackson2HttpMesageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 // Com essa annotation "@Configuration" o spring saberá que precisa executar essa classe no carregamento
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMesageConverter());
+    }
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         // Via EXTENSION
@@ -30,6 +40,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
     }
 }
